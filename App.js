9974393bb6a257cd79dc322864fd192eb9011012
Pasindu Perera <120162481+Pas1ya02@users.noreply.github.com>
2025-04-13 19:19:50 +0530

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,73 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import "./global.css"
+import LoginForm from './components/LoginForm';
+import RegistrationForm from './components/RegistrationForm';
+import Register from './components/Register';
+import ResetPassword from './components/ResetPassword';
+import OTPVerification from './components/OTPVerification';
+import DriverHome from './components/DriverHome';
+import StatsPage from './components/StatsPage';
+import MyOrder from './components/MyOrder';
+import ProofOfDelevery from './components/ProofOfDeliver.jsx';
+import RiderConfirmed from './components/RiderConfirmed';
+import DeliveryRequestModal from './components/DeliveryRequestModal';
+import OrderPreview from './components/OrderPreview';
+import LiveTrack from './components/LiveTrack';
+import CustomerConfiremed from './components/CustomerConfiremed';
+import DeliveryComplete from './components/DeliveryComplete';
+import ChatList from './components/ChatList';
+import ResheduleDelivery from './components/ResheduleDelivery';
+import ChatOp from './components/chatOp';
+import Profile from './components/Profile.jsx';
+
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.container}>
+
+        <LoginForm />
+        {/* <RegistrationForm/> */}
+        {/* <Register /> */}
+        {/* <ResetPassword /> */}
+        {/* <OTPVerification/> */}
+        {/* <ResetPassword /> */}
+
+        {/* <DriverHome /> */}
+        {/* <StatsPage/> */}
+
+        {/* <MyOrder/> */}
+        {/* <OrderPreview/> */}
+     {/* <ResheduleDelivery/> */}
+        {/* <LiveTrack/> */}
+
+        {/* <DeliveryRequestModal/> */}
+        {/* <RiderConfirmed/> */}
+        {/* <CustomerConfiremed/> */}
+
+        {/* <DeliveryComplete /> */}
+        {/* <ProofOfDelevery/> */}
+        {/* <ChatList/> */}
+         {/* <ChatOp/> */}
+         {/* <Profile/> */}
+
+        <StatusBar style="auto" />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  safeArea: {
     flex: 1,
     backgroundColor: '#fff',
+  },
+  container: {
+    flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
   },
 });
+
